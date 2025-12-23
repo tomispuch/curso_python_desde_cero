222bfa544253911ff1dfc41583d33e0bd9408e89
tomispuch <tomasspuch@gmail.com>
2025-12-23 16:43:33 -0300

    Agrego clase de input y ejercicios del bloque 1

---
+++ 01_basic/05_input.py
@@ -0,0 +1,13 @@
+# Input de datos del usuario
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+print(f"Hola {nombre}, tienes {edad} años.")
+
+# El input siempre devuelve un string
+print(f"El tipo de dato de edad es: {type(edad)}")
+
+# Si queremos usar la edad como un numero, debemos castear
+# Por ejemplo, para sumar 1 a la edad
+edad_num = int(edad)
+nueva_edad = edad_num + 1
+print(f"El próximo año tendrás {nueva_edad} años.")
---
+++ 01_basic/ejercicios.py
@@ -0,0 +1,61 @@
+
+print("\nEjercicio 1: Imprimir mensajes")
+print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
+
+### Completa aquí
+print("Mi nombre es Tomas y vivo en Buenos aires")
+
+print("--------------")
+
+print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
+print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
+a = 15
+b = 3.14159
+c = "Hola mundo"
+d = True
+e = None
+
+### Completa aquí
+print(f"El tipo de dato de a es: {type(a)}")
+print(f"El tipo de dato de b es: {type(b)}")
+print(f"El tipo de dato de c es: {type(c)}")
+print(f"El tipo de dato de d es: {type(d)}")
+print(f"El tipo de dato de e es: {type(e)}")
+
+
+print("--------------")
+
+print("\nEjercicio 3: Casting de tipos")
+print("Convierte la cadena \"12345\" a un entero y luego a un float.")
+print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
+
+### Completa aquí
+cadena = "12345"
+entero = int(cadena)
+flotante = float(cadena)
+
+
+
+print("--------------")
+
+print("\nEjercicio 4: Variables")
+print("Crea variables para tu nombre, edad y altura.")
+print("Usa f-strings para imprimir una presentación.")
+
+# "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
+
+### Completa aquí
+nombre = "Tomas"
+edad = 21
+altura = 1.80
+
+print(f"Hola! Me llamo {nombre} y tengo {edad} años, mido {altura} metros")
+
+
+print("--------------")
+
+print("\nEjercicio 5: Números")
+print("1. Crea una variable con el número PI (sin asignar una variable)")
+print("2. Redondea el número con round()")
+print("3. Haz la división entera entre el número que te salió y el número 2")
+print("4. El resultado debería ser 1")
