93faa6e780247b22c694b103d9fe4b85e35db53a
tomispuch <tomasspuch@gmail.com>
2025-12-26 12:25:30 -0300

    Pruebas con PUT GET POST

--- 06_request_ia_dates/02_request.py
+++ 06_request_ia_dates/02_request.py
@@ -31,3 +31,56 @@ try:
     print(json_data)
 except Exception as e:
     print("Ocurrió un error al hacer la petición:", e)
+
+
+#2, Con dependencias externas (usando requests)
+import requests
+
+response = requests.get(api_posts) # Hacer una petición GET a la URL (PARECIDO A SpringBoot)
+
+if response.status_code == 200: # Verificar que la respuesta sea exitosa (código 200)
+    json_data = response.json() # Obtener los datos en formato JSON directamente
+    print(json_data)
+else:
+    print("Error en la petición:", response.status_code)
+
+# Requests maneja automáticamente la apertura y cierre de la conexión
+# También maneja errores de red y HTTP de manera más sencilla
+# Además, es más fácil de usar y leer el código
+# Por estas razones, requests es la opción preferida para hacer peticiones HTTP en Python  
+
+#Hacer un post
+post_data = {
+    "title": "Prueba de POST",
+    "body": "Este es el contenido de la prueba de POST",
+    "userId": 1
+}
+response = requests.post(api_posts, json=post_data) # Hacer una petición POST
+
+
+if response.status_code == 201: # Verificar que la respuesta sea exitosa (código 201 para creación)
+    json_data = response.json()
+    print("POST creado:", json_data)
+else:
+    print("Error en la petición POST:", response.status_code)
+
+
+#Hacer un put
+api_posts_put = "https://jsonplaceholder.typicode.com/posts/1" # URL para actualizar el post con ID 1
+
+
+put_data = {
+    "id": 1,
+    "title": "Prueba de PUT",
+    "body": "Este es el contenido de la prueba de PUT",
+    "userId": 1
+}
+response = requests.put(api_posts_put, json=put_data) # Hacer una petición PUT
+if response.status_code == 200: # Verificar que la respuesta sea exitosa (código 200)
+    json_data = response.json()
+    print("POST actualizado:", json_data)
+else:
+    print("Error en la petición PUT:", response.status_code)
+
+
+#Usar la API de OpenAI
